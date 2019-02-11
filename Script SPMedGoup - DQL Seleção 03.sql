d3016d50daf185c72e9b6a1eb655c0c8a8f9510b
Odirlei Sabella de Assis <32184284831@info.local>
2019-02-11 17:20:01 -0200

    inclusao do metodo procedure

--- Script SPMedGoup - DQL Seleção 03.sql
+++ Script SPMedGoup - DQL Seleção 03.sql
@@ -6,9 +6,17 @@ SELECT CONVERT(VARCHAR(10), data_nascimento, 110) AS [MM-DD-YYYY] from PRONTUARI
 
 select * from MEDICOS
 
-select count(*) from MEDICOS inner join ESPECIALIDADES
-on MEDICOS.ID_ESPECIALIDADE = ESPECIALIDADES.ID and
-ESPECIALIDADES.NOME = 'Psiquiatria'
+create procedure Medicos_Especialidade
+	@NOME varchar(100)
+as
+begin
+	select count(*) from MEDICOS inner join ESPECIALIDADES
+	on MEDICOS.ID_ESPECIALIDADE = ESPECIALIDADES.ID and
+	ESPECIALIDADES.NOME = @NOME
+end;
+
+drop procedure Medicos_Especialidade
+
+exec Medicos_Especialidade 'Psiquiatria';
 
 SELECT FLOOR(DATEDIFF(DAY, data_nascimento, GETDATE()) / 365.25) from prontuario
-select * from PRONTUARIO order by id
